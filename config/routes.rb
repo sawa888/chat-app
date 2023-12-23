1e497c6ff52c91d49279e56c843b383f6d07f9de
sawa888 <oblige.tokyo@docomo.ne.jp>
2023-12-23 15:12:29 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
- root to: "messages#index"
- devise_for :users
+  devise_for :users
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
+
 end
