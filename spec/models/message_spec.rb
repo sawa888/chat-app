dfb68093a59d3d9382e3796092058a48efdbce57
sawa888 <oblige.tokyo@docomo.ne.jp>
2023-12-30 12:24:44 +0900

    各モデルのテスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -9,10 +9,35 @@ RSpec.describe Message, type: :model do
   describe 'メッセージ投稿' do
     context 'メッセージが投稿出来る場合' do
       it 'contentとomageが存在していれば保存できる' do
-
+        expect(@message).to be_valid
       end
       it 'contentが空でも保存できる' do
+        @message.content = ""
+        expect(@message).to be_valid
+      end
+      it 'imageが空でも保存できる' do
+        @message.image = nil
+        expect(@message).to be_valid
+      end
+    end
+    context 'メッセージが投稿できない場合' do
+      it 'contentとimageが空では保存出来ない' do
+        @message.content = ''
+        @message.image = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'roomが紐づいていないと保存出来ない' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room must exist")
+      end
+      it 'userが紐づいてないと保存出来ない' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User must exist")  
+      end
     end
   end
-
 end
+
