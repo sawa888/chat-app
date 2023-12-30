deeae83c6e99aad3bfbd852140e69f5bc2e659fc
sawa888 <oblige.tokyo@docomo.ne.jp>
2023-12-30 11:56:03 +0900

    一次保存

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿出来る場合' do
+      it 'contentとomageが存在していれば保存できる' do
+
+      end
+      it 'contentが空でも保存できる' do
+    end
+  end
+
 end
