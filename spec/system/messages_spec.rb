02767ddbb4b42c4f8ac7a9890a36316ca8963fc3
sawa888 <oblige.tokyo@docomo.ne.jp>
2024-02-10 19:38:39 +0900

    結合テストの修正

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -58,33 +58,39 @@ RSpec.describe 'メッセージ投稿機能', type: :system do
       # 画像選択フォームに画像を添付する
       attach_file('message[image]', image_path, make_visible: true)
       # 送信した画像の値がDBに保存されていることを確認する
-      binding.pry
       expect{
         find('input[name="commit"]').click 
         sleep 1
       }.to change { Message.count }.by(1)
       # 投稿一覧画面に遷移していることを確認する
-      expect(page)
-
+     
+      expect(current_path).to eq(room_messages_path(@room_user.room))
       # 送信した画像がブラウザに表示されていることを確認する
-
+      
+      expect(page).to have_selector('img')
     end
      it 'テキストと画像の投稿に成功する' do
       # サインインする
-     
+      sign_in(@room_user.user)
       # 作成されたチャットルームへ遷移する
-     
+      click_on(@room_user.room.name)
       # 添付する画像を定義する
-      
+      image_path = Rails.root.join('public/images/test_image.png')
       # 画像選択フォームに画像を添付する
-
+      attach_file('message[image]', image_path, make_visible: true)
       # 値をテキストフォームに入力する
-
+      post = 'テスト'
+      fill_in 'message_content', with: post
       # 送信した値がDBに保存されていることを確認する
-
+      expect {
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change {Message.count}.by(1)
       # 送信した値がブラウザに表示されていることを確認する
+      expect(page).to have_content(post)
 
       # 送信した画像がブラウザに表示されていることを確認する
+      expect(page).to have_selector('img')
 
      end
   end
